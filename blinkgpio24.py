7d7289b2f44ad9f78a949aaa418fbbdf1657d062
jbrucepayne <jbrucepayne@gmail.com>
2019-01-05 23:39:07 -0700

    Create blinkgpio24.py

---
+++ blinkgpio24.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.OUT)
+while True:
+     GPIO.output(24, True)
+     time.sleep(1)
+     GPIO.output(24, False)
+     time.sleep(1)
