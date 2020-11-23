28daf113b7323feda6965fd88aa40e5e54a9277c
Bruce Payne <jbrucepayne@gmail.com>
2020-11-23 13:48:19 -0700

    Some other experiments.

--- circuitpython/code-multiscenario.py
+++ circuitpython/code-multiscenario.py
@@ -9,6 +9,26 @@ import audiobusio            # use the microphone for input
 import math                  # for microphone example
 import array                 # for microphone example
 
+##### Accelerometer?
+import busio
+import adafruit_lis3dh
+i2c = busio.I2C(board.ACCELEROMETER_SCL, board.ACCELEROMETER_SDA)
+int1 = digitalio.DigitalInOut(board.ACCELEROMETER_INTERRUPT)
+lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, address=0x19, int1=int1)
+# Set range of accelerometer (can be RANGE_2_G, RANGE_4_G, RANGE_8_G or RANGE_16_G).
+lis3dh.range = adafruit_lis3dh.RANGE_2_G
+# Set tap detection.  The first parameter is a value:
+#  - 0 = Disable tap detection.
+#  - 1 = Detect single taps.
+#  - 2 = Detect double taps.
+# The second parameter is the threshold and a higher value means less sensitive
+# tap detection.  Note the threshold should be set based on the range above:
+#  - 2G = 40-80 threshold
+#  - 4G = 20-40 threshold
+#  - 8G = 10-20 threshold
+#  - 16G = 5-10 threshold
+lis3dh.set_tap(2, 80)
+
 # my routines that are set up for ease of use and consistency
 import local
 
@@ -23,12 +43,23 @@ lightSensorPin=board.LIGHT
 thermistorPin=board.TEMPERATURE
 strandPixelPin=board.A2
 thermocoupleInputPin=board.A1
+audioOutPin=board.SPEAKER
+motionSensorPin = board.A6
+analogVelostatPin = board.A3
 
 led = digitalio.DigitalInOut(ledOutputPin)
 led.direction = digitalio.Direction.OUTPUT
 
 relay = digitalio.DigitalInOut(relayPin)
 relay.switch_to_output()
+
+motionSensor = digitalio.DigitalInOut(motionSensorPin)
+motionSensor.switch_to_input()
+lastMotionSensorValue = motionSensor.value
+motionSensorStartTime = time.time()
+motionSensorEndTime = motionSensorStartTime
+
+analogVelostat = analogio.AnalogIn(analogVelostatPin)
 
 light = analogio.AnalogIn(lightSensorPin)
 
@@ -82,7 +113,30 @@ def normalized_rms(values):
 
     return math.sqrt(samples_sum / len(values))
 
+###########   Begin audio play bit
+# Enable the speaker
+speaker_enable = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
+speaker_enable.direction = digitalio.Direction.OUTPUT
+speaker_enable.value = True
 
+try:
+    from audiocore import WaveFile
+except ImportError:
+    from audioio import WaveFile
+
+try:
+    from audioio import AudioOut
+except ImportError:
+    try:
+        from audiopwmio import PWMAudioOut as AudioOut
+    except ImportError:
+        pass  # not always supported by every board!
+
+wave_file = open("StreetChicken.wav", "rb")
+wave = WaveFile(wave_file)
+audio = AudioOut(audioOutPin)
+
+###########   End audio play bit
 def mean(values):
     return sum(values) / len(values)
 
@@ -108,21 +162,12 @@ def setPixelColorsFromVolume():
     strandpixels.show()
 ##################### END FROM AUDIO MICROPHONE EXAMPLE
 
-def color_for_temperature (temperature):
-    if temperature >= 60:
-        return (0,255,0)
-    if temperature >= 50:
-        return (0,0,255)
-    if temperature >= 40:
-        return (255,255,0)
-    if temperature < 40:
-        return (255,0,0)
 
     # White is default if we don't know what else to do!
     return (255,255,255)
 
 def setPixelColorsByTemperature(temperature):
-    strandpixels.fill(color_for_temperature(temperature))
+    strandpixels.fill(local.color_for_temperature(temperature))
     strandpixels.show()
 
 while True:
@@ -147,14 +192,48 @@ while True:
     temp_f = temp_c * 9 / 5 + 32
     #print("Thermistor Temperature is:   %f F and %f C" % (temp_f, temp_c))
 
+    if (analogVelostat.value > 100):
+       onboardpixels[8] = (255,0,0)
+       onboardpixels[7] = (255,0,0)
+       onboardpixels[6] = (255,0,0)
+       onboardpixels.show()
+    print ("Velostat", analogVelostat.value)
+
     ###### Temperature from thermocouple
     thermistor_c = (local.get_voltage(ad8495) - 1.25) / 0.005
     thermistor_f = thermistor_c * 9 / 5 + 32
-    print ("Thermocouple Temperature is: %f F" % (thermistor_f))
-    setPixelColorsByTemperature(thermistor_f)
+    #print ("Thermocouple Temperature is: %f F" % (thermistor_f))
+    #setPixelColorsByTemperature(thermistor_f)
 
 
     #### Audio example, make lights light VU meter
-    #setPixelColorsFromVolume()
+    setPixelColorsFromVolume()
 
-    time.sleep(60.0)
+    #print (motionSensor.value)
+    if (lastMotionSensorValue != motionSensor.value):
+        if (lastMotionSensorValue):
+            motionSensorEndTime = time.time()
+            print ("Elapsed time: (s)",motionSensorEndTime - motionSensorStartTime)
+        else:
+            motionSensorStartTime = time.time()
+            print ("Start time: ", motionSensorStartTime, " Elapsed since End", motionSensorStartTime - motionSensorEndTime)
+    lastMotionSensorValue = motionSensor.value
+
+    #audio.play(wave)
+    # This allows you to do other things while the audio plays!
+    #t = time.monotonic()
+    #while time.monotonic() - t < 6:
+    #    pass
+    x, y, z = [
+        value / adafruit_lis3dh.STANDARD_GRAVITY for value in lis3dh.acceleration
+    ]
+    #print("x = %0.3f G, y = %0.3f G, z = %0.3f G" % (x, y, z))
+
+    if lis3dh.tapped:
+        for i in range(numPixels):
+            strandpixels[i] = (255,255,255)
+        strandpixels.show()
+        print("Tapped!")
+        time.sleep(1.0)
+
+    time.sleep(1)
